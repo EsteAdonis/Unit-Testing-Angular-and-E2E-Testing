c39d78ca25398b523f381115c8cdc48550e0c349
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-04-28 14:36:16 -0600

    Testing HttpError

--- src/app/services/api.service.ts.service.spec.ts
+++ src/app/services/api.service.ts.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { ApiServiceTsService } from './api.service.ts.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TagInterface } from './tag.interface';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('ApiServiceTsService', () => {
   let apiService: ApiServiceTsService;
@@ -53,7 +54,26 @@ describe('ApiServiceTsService', () => {
     const req = httpTestingController.expectOne('http://localhost:3004/tags');
     req.flush({ id: '1', name: 'foo' })
     expect(req.request.method).toEqual('POST');
-    expect(req.request.body).toEqual({name: 'foo'});
-  })
+    expect(req.request.body).toEqual({ name: 'foo' });
+  });
+
+  it('Should return an error', () => {
+    let actualError: HttpErrorResponse | undefined;
+    apiService.createTag('foo').subscribe({
+      next: () => fail('Success should not be called'),  // fail => Explicity mark a spec as fail
+      error: err => actualError = err
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:3004/tags');
+    req.flush('Server error', {
+      status: 422,
+      statusText: 'Unprocessable entity'
+    });
+    if (!actualError) {
+      throw new Error('Error needs to be defined');
+    }
+    expect(actualError.status).toEqual(422);
+    expect(actualError.statusText).toEqual('Unprocessable entity');
+  });
 
 });
