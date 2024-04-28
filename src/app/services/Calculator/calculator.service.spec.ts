2beda261bc4d7c2e22326843d4aa40e3ee75b68a
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-04-28 17:56:33 -0600

    Using only mockLoggerService

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -1,29 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 import { CalculatorService } from './calculator.service';
-import { LoggerService } from '../Logger/logger.service';
 
 describe('CalculatorService', () => {
-  let service: CalculatorService;
+  let calculatorService: CalculatorService;
   let mockLoggerService: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     mockLoggerService = jasmine.createSpyObj('LoggerService', ['log']);
-    service = new CalculatorService(mockLoggerService);
+    calculatorService = new CalculatorService(mockLoggerService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(calculatorService).toBeTruthy();
   });
 
   it('Should return the sum of two numbers', () => {
-    let result = service.add(5, 15);
+    let result = calculatorService.add(5, 15);
     expect(result).toEqual(20);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);    
   });
 
   it('Should return the subtraction of two numbers', () => {
-    let result = service.subtract(15, 5)
+    let result = calculatorService.subtract(15, 5)
     expect(result).toBeGreaterThan(0);
     expect(mockLoggerService.log).toHaveBeenCalledTimes(1);
   })
