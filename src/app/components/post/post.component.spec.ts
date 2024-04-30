47a3c8f2341e1aa075c5dce811f105b907245289
EsteAdonis 2025 <julio.e.ochoa.p@gmail.com>
2024-04-29 23:40:09 -0600

    Applying the TestBed.ConfigurationTestingModule for PostComponent

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -1,8 +1,22 @@
 import { first } from "rxjs";
 import { Post } from "../../models/Post";
 import { PostComponent } from "./post.component";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 describe('Post Component', () => {
+  let component: PostComponent;
+  let fixture: ComponentFixture<PostComponent>;
+  
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PostComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PostComponent);
+    component = fixture.componentInstance;
+  });    
+
   it('Should raise an event  when the delete post is clicked', () => {
     const comp = new PostComponent();
     const post: Post = { id: 1, body: 'Body 1', title: 'Adonis' };
